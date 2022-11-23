b3b86219280ca5ab79afe5826b2124b9516a1fff
tjas98 <45484411+tjas98@users.noreply.github.com>
2022-11-23 11:55:54 +0100

    Update db.js

--- api/controllers/db.js
+++ api/controllers/db.js
@@ -4,8 +4,10 @@ const url = "mongodb://localhost:27017";
 
 const publicUrl = 'mongodb+srv://urnik:UrnikPreseren@urnik.vpq0cxv.mongodb.net/Urnik'
 
+/*
 mongoose.connect(publicUrl)
 const db = mongoose.connection
 db.once('open', _ => {
     console.log("MONGO Connected")
 })
+*/
