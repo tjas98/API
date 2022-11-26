841a7c6fdbdcfcf7ae706d86e337850877d856b7
tjas98 <45484411+tjas98@users.noreply.github.com>
2022-11-26 10:43:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,11 +15,14 @@ var indexApi = require('./api/routes/index');
  
 app.use(bodyParser.json()) 
 
+/*
+
 app.use('/', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
+*/
 
 app.get('/', (req, res) => {
     res
