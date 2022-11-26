315efde2a36571f8f021e7f772c3932a35961790
tjas98 <45484411+tjas98@users.noreply.github.com>
2022-11-26 10:57:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(bodyParser.json())
 app.use('/', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.redirect("https://" + res.headers.host + res.url);
     next();
 });
 
